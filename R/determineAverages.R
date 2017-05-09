70188b6bb9c441b2630d7b815656747c1ba43434
Roelant Ossewaarde <roelanto@gmail.com>
2017-05-09 20:59:03 +0200

    Added documentation, exported function.

--- R/determineAverages.R
+++ R/determineAverages.R
@@ -1,3 +1,12 @@
-determineAverages <- function(wav, bands, freqrange=c(0.4, 200), start=-1, end=-1) {
-  return(findValInSpec(object=wav, type="mean", bands=bands, freqrange=freqrange, start=start, end=end))
+#' determine the averages, to facilitate noise profile creation.
+#'
+#' @param object the WAV-object
+#' @param bands the bands in which to compute the spectral envelope
+#' @param freqrange the frequency range to analyze.
+#' @param start the start position in seconds
+#' @param end the end position in seconds
+#' @export
+#'
+determineAverages <- function(object, bands, freqrange=c(0.4, 200), start=-1, end=-1) {
+  return(findValInSpec(object=object, type="mean", bands=bands, freqrange=freqrange, start=start, end=end))
 }
